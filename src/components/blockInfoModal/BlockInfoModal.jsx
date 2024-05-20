0a297914810a6f00b606f8193f8c0e0eeddab406
Paula <paulamarcon1710@gmail.com>
2024-05-20 17:06:41 -0300

    Saco a un archivo a parte los styles del modal de informacion de los bloques

--- src/components/blockInfoModal/BlockInfoModal.jsx
+++ src/components/blockInfoModal/BlockInfoModal.jsx
@@ -1,45 +1,17 @@
+import "./styles.css";
+
 const BlockInfoModal = ({ blockType, blockInfo, mouseClickPosition }) => {
-  const thoughtBubbleStyle = {
-    position: "absolute",
+  const dynamicStyle = {
     top: mouseClickPosition.y - 150,
     left: mouseClickPosition.x - 100,
-    background: "#17a2b8",
-    borderRadius: "15px",
-    padding: "20px",
-    color: "white",
-    width: "200px",
-    zIndex: 9999,
-  };
-
-  const beforeAfterStyle = {
-    content: '""',
-    position: "absolute",
-    background: "#17a2b8",
-    borderRadius: "50%",
-  };
-
-  const beforeStyle = {
-    ...beforeAfterStyle,
-    width: "20px",
-    height: "20px",
-    bottom: "-10px",
-    left: "20px",
-  };
-
-  const afterStyle = {
-    ...beforeAfterStyle,
-    width: "12px",
-    height: "12px",
-    bottom: "-25px",
-    left: "40px",
   };
 
   return (
-    <div style={thoughtBubbleStyle}>
+    <div className="thoughtBubbleStyle" style={dynamicStyle}>
       <h5>{blockType}</h5>
       <p>{blockInfo}</p>
-      <div style={beforeStyle}></div>
-      <div style={afterStyle}></div>
+      <div className="beforeStyle beforeAfterStyle"></div>
+      <div className="afterStyle beforeAfterStyle"></div>
     </div>
   );
 };
